c47a9d44cfa4f8457cd768ce7e273a3583bc30bc
joelshibu99 <joelshibu100@gmail.com>
2026-02-03 11:18:48 +0530

    Create sidebarConfig.js

---
+++ lms_frontend/src/components/layout/sidebarConfig.js
@@ -0,0 +1,32 @@
+import DashboardIcon from "@mui/icons-material/Dashboard";
+
+export const sidebarConfig = {
+  SYSTEM_ADMIN: [
+    {
+      label: "Dashboard",
+      path: "/system/dashboard",
+      icon: <DashboardIcon />,
+    },
+  ],
+  COLLEGE_ADMIN: [
+    {
+      label: "Dashboard",
+      path: "/college/dashboard",
+      icon: <DashboardIcon />,
+    },
+  ],
+  TEACHER: [
+    {
+      label: "Dashboard",
+      path: "/teacher/dashboard",
+      icon: <DashboardIcon />,
+    },
+  ],
+  STUDENT: [
+    {
+      label: "Dashboard",
+      path: "/student/dashboard",
+      icon: <DashboardIcon />,
+    },
+  ],
+};
